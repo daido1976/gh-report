089ed9215059e25118eb9a178c9c4af370b5e3d7
daido1976 <daido1976@gmail.com>
2022-07-23 07:45:38 +0900

    Revert "Dont use inline snapshot testing"
    This reverts commit 83d536ac2119feb10b28e376aae469daa08c2cb1.

--- src/presenter.rs
+++ src/presenter.rs
@@ -19,12 +19,27 @@ pub fn to_string_pretty(my_contributions: adapter::MyContributions) -> String {
 #[cfg(test)]
 mod test {
     use super::*;
-    use insta::assert_display_snapshot;
+    use insta::assert_snapshot;
 
     #[test]
     fn test_to_string_pretty() {
         let cc = serde_json::from_str(include_str!("fixtures/myContributions.json")).unwrap();
         let actual = to_string_pretty(cc);
-        assert_display_snapshot!(actual);
+        assert_snapshot!(
+            actual,
+            @r###"
+
+        ### rust-lang/rust
+
+        - [Cannot build on Fedora: wrong CPUTYPE?](https://github.com/rust-lang/rust/issues/1218) **open!**
+        - [rustc: Fix position of diagnostic highlight lines](https://github.com/rust-lang/rust/issues/1219) **closed!**
+        - [Add float support to #fmt.](https://github.com/rust-lang/rust/pull/1168) **merged!**
+        - [Prohibit in-scope consts from use as variable names in binders, like nullary tags](https://github.com/rust-lang/rust/pull/1193) **merged!**
+
+        ### daido1976/terakoya
+
+        - [From now to Vercel](https://github.com/daido1976/terakoya/pull/52) **merged!**
+        "###
+        );
     }
 }
