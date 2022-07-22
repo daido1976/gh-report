9934bdb07fe8bbb81f7251b7bccb8482d0dcf949
daido1976 <daido1976@gmail.com>
2022-07-23 07:52:07 +0900

    Fixed a problem with the formatter causing test failed

--- src/presenter.rs
+++ src/presenter.rs
@@ -19,27 +19,17 @@ pub fn to_string_pretty(my_contributions: adapter::MyContributions) -> String {
 #[cfg(test)]
 mod test {
     use super::*;
-    use insta::assert_snapshot;
+    use insta::assert_debug_snapshot;
 
     #[test]
     fn test_to_string_pretty() {
         let cc = serde_json::from_str(include_str!("fixtures/myContributions.json")).unwrap();
         let actual = to_string_pretty(cc);
-        assert_snapshot!(
+        // I would like to use assert_display_snapshot, but the leading whitespace causes problems, so I use assert_debug_snapshot instead.
+        // See. https://github.com/mitsuhiko/insta/issues/117
+        assert_debug_snapshot!(
             actual,
-            @r###"
-
-        ### rust-lang/rust
-
-        - [Cannot build on Fedora: wrong CPUTYPE?](https://github.com/rust-lang/rust/issues/1218) **open!**
-        - [rustc: Fix position of diagnostic highlight lines](https://github.com/rust-lang/rust/issues/1219) **closed!**
-        - [Add float support to #fmt.](https://github.com/rust-lang/rust/pull/1168) **merged!**
-        - [Prohibit in-scope consts from use as variable names in binders, like nullary tags](https://github.com/rust-lang/rust/pull/1193) **merged!**
-
-        ### daido1976/terakoya
-
-        - [From now to Vercel](https://github.com/daido1976/terakoya/pull/52) **merged!**
-        "###
+            @r###""\n### rust-lang/rust\n\n- [Cannot build on Fedora: wrong CPUTYPE?](https://github.com/rust-lang/rust/issues/1218) **open!**\n- [rustc: Fix position of diagnostic highlight lines](https://github.com/rust-lang/rust/issues/1219) **closed!**\n- [Add float support to #fmt.](https://github.com/rust-lang/rust/pull/1168) **merged!**\n- [Prohibit in-scope consts from use as variable names in binders, like nullary tags](https://github.com/rust-lang/rust/pull/1193) **merged!**\n\n### daido1976/terakoya\n\n- [From now to Vercel](https://github.com/daido1976/terakoya/pull/52) **merged!**\n""###
         );
     }
 }
